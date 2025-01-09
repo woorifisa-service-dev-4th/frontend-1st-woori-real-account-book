8071662e0db21c854b416f8e0b741841ca335b09
june0216 <ziyun1612@ewhain.net>
2025-01-09 10:19:15 +0900

    feat : 원차트 생성

--- public/js/categoryCircle.js
+++ public/js/categoryCircle.js
@@ -1,3 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
+// 데이터 및 옵션 설정
+const expData = {
+    labels: ["식비", "쇼핑", "공과금", "교통비", "경조사"],
+    datasets: [
+        {
+            data: [40, 25, 15, 10, 10], // 각 카테고리의 비율
+            borderWidth: 2,
+            hoverBorderWidth: 3,
+            backgroundColor: [
+                "#2141E8", // 식비
+                "#2447FF", // 쇼핑
+                "#506CFF", // 공과금
+                "#6C84FF", // 교통비
+                "#9AAAFF", // 경조사
+            ],
+            fill: true,
+        },
+    ],
+};
 
+const options = {
+    responsive: true,
+    maintainAspectRatio: false, // 기본 비율 유지하지 않음
+    plugins: {
+        legend: {
+            display: false,
+            position: "right", // 범례를 오른쪽에 배치
+        },
+    },
+};
+
+// 차트 생성
+const ctx = document.getElementById("myDoughnutChart").getContext("2d");
+new Chart(ctx, {
+    type: "doughnut",
+    data: expData,
+    options: options,
 });
